3ca0c543dbd02af0a2a5d586426621a5387dcf4a
Sebamun <SebastianAmundsen99@gmail.com>
2022-03-31 10:54:15 +0200

    get the correct numbers for score divergence

--- Oblig_1/PageRank_iterations.c
+++ Oblig_1/PageRank_iterations.c
@@ -9,40 +9,48 @@
 void PageRank_iterations(int n, int *row_ptr, int *col_idx, double *val,
   double d, double epsilon, double *scores){
 
-    printf("------------------ \n");
     double *x;
     x = (double*) malloc(n * sizeof(double));
-    double *diff;
-    diff = (double*) malloc(n * sizeof(double));
-    double crit = 1.0;
+    //double *diff;
+    //diff = (double*) malloc(n * sizeof(double));
 
     double term;
-    double x_upd[n];
-    int ones[n];
+    //int ones[n];
     scores = (double*) malloc(n * sizeof(double));
     // Initialize:
-    //term[0] = 0;
     for (int i = 0; i<n; i++){
       x[i]=1.0/n;
-      ones[i] = 1.0;
     }
 
-    while (crit>epsilon) {
-      /* code */
+    for (int m = 0; m < 100; m++){
+    //while (crit>epsilon) {
       for (int i=0; i<n; i++){
-        term = 0;
+        term = 0.;
         for (int j=row_ptr[i]; j<row_ptr[i+1]; j++){
-          term += val[j] * x[col_idx[j]]; // Sparse matrix multiplication.
+          term = term + val[j] * x[col_idx[j]]; // Sparse matrix multiplication.
         }
-        scores[i] = term;
-        printf("%f \n", scores[i]);
-        diff[i] = fabs(scores[i] - x[i]);
-        //printf("%f", diff[i])
+        scores[i] =  d*term;
+        printf("%f\n", scores[i]);
+        }
+        for (int i =0; i<n; i++) x[i] = scores[i];
+        printf("---------\n");
       }
-      crit = largest(diff, n);
-      //printf("%f \n", crit);
-      x = scores;
-      printf("---- \n");
-  }
+        //for (int i=0; i < n; i++) printf("%f \n", x[i]);
+    }
 
-}
+
+
+
+
+        //printf("%f \n", scores[i]);
+        //diff[i] = fabs(scores[i] - x[i]);
+        //x[i] = scores[i];
+
+        //printf("%f", diff[i])
+
+      //crit = largest(diff, n);
+      //printf("%f \n", crit);
+      //x = scores;
+      //printf("---- \n");
+    //}
+  //}
