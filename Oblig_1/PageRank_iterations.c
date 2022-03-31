4a97da677b3764e9d1ead595b84845036284716f
Sebamun <SebastianAmundsen99@gmail.com>
2022-03-31 11:26:40 +0200

    works now without dangling webpages

--- Oblig_1/PageRank_iterations.c
+++ Oblig_1/PageRank_iterations.c
@@ -11,32 +11,39 @@ void PageRank_iterations(int n, int *row_ptr, int *col_idx, double *val,
 
     double *x;
     x = (double*) malloc(n * sizeof(double));
-    //double *diff;
-    //diff = (double*) malloc(n * sizeof(double));
-
-    double term;
+    double *diff;
+    diff = (double*) malloc(n * sizeof(double));
+    double crit = 1.0;
+    int k=0; // How many iterations we need before convergence.
+    double term; // Here we store the vector matrix multiplication.
     //int ones[n];
     scores = (double*) malloc(n * sizeof(double));
     // Initialize:
     for (int i = 0; i<n; i++){
-      x[i]=1.0/n;
+      x[i]=1.0/n; // Initial "guess".
     }
 
-    for (int m = 0; m < 100; m++){
-    //while (crit>epsilon) {
+    while (crit>epsilon) {
+      k++; // Add to counter.
       for (int i=0; i<n; i++){
-        term = 0.;
+        term = 0.; // Reset vector matrix multiplication.
         for (int j=row_ptr[i]; j<row_ptr[i+1]; j++){
           term = term + val[j] * x[col_idx[j]]; // Sparse matrix multiplication.
         }
-        scores[i] =  d*term;
-        printf("%f\n", scores[i]);
+        scores[i] =  d*term; // The PageRank algorithm.
+        // Our stopping criteria is given by the difference between x[i]
+        // and scores[i]:
+        diff[i] = fabs(scores[i] - x[i]);
+        crit = largest(diff, n); // Find the largest difference.
         }
+        // We now update our x[i] according to the new calculated scores[i].
+        // (Note that this has to be done iteratively, as just
+        // putting the pointers equal to each other dosent work.)
         for (int i =0; i<n; i++) x[i] = scores[i];
-        printf("---------\n");
       }
-        //for (int i=0; i < n; i++) printf("%f \n", x[i]);
-    }
+      for (int i=0; i < n; i++) printf("%f \n", scores[i]); // Converged score.
+      }
+
 
 
 
