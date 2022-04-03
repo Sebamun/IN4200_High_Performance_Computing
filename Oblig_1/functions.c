4d03b7b1c6add0b3dd95ee5d637d02236894f85d
Sebamun <SebastianAmundsen99@gmail.com>
2022-04-03 13:54:38 +0200

    fixed functions.c

--- Oblig_1/functions.c
+++ Oblig_1/functions.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 void swapper(int* xp, int* yp)
-{// Used to swap position of two elements in array.
+{// Used to swap position of two integer elements in array.
     int t = *xp;
     *xp = *yp;
     *yp = t;
 }
 
 void swapper_2(double* xp, double* yp)
-{
+{ // Used to swap doubles.
     double t = *xp;
     *xp = *yp;
     *yp = t;
 }
 
-
-
 void Sort(int arr[], int arr_2[], int n)
-{ // This function sorts two array, one in descending order, the other follows
+{ // This function sorts two arrays, one in descending order, the other follows
   // the same changes as in the first array.
-    int i, j, min_idx;
+    int i;
+    int j;
+    int min_idx;
 
     // One by one move boundary of unsorted subarray
     for (i = 0; i < n; i++) { // Run through the whole array.
@@ -29,8 +29,7 @@ void Sort(int arr[], int arr_2[], int n)
             if (arr[j] < arr[min_idx])
                 min_idx = j;
 
-        // Swap the found minimum element
-        // with the first element
+        // Swap the found mimimum element with the first element.
         swapper(&arr[min_idx], &arr[i]);
         swapper(&arr_2[min_idx], &arr_2[i]);
     }
@@ -38,33 +37,29 @@ void Sort(int arr[], int arr_2[], int n)
 
 void Sort_2(double arr[], int arr_2[], int n)
 {
-    int i, j, min_idx; // Iterative indexes + min index.
+    int i;
+    int j;
+    int max_idx;
 
     // One by one move boundary of unsorted subarray
     for (i = 0; i < n; i++) { // Run through the whole array.
-        // Find the minimum element in unsorted array
-        min_idx = i;
+        // Find the maximum element in unsorted array
+        max_idx = i;
         for (j = i + 1; j < n; j++)
-            if (arr[j] > arr[min_idx])
-                min_idx = j;
+            if (arr[j] > arr[max_idx])
+                max_idx = j; // New max index.
 
-        // Swap the found minimum element
-        // with the first element
-        swapper_2(&arr[min_idx], &arr[i]);
-        swapper(&arr_2[min_idx], &arr_2[i]);
+        // Swap the found maximum element with the first element.
+        swapper_2(&arr[max_idx], &arr[i]);
+        swapper(&arr_2[max_idx], &arr_2[i]);
     }
 }
 
 double largest(double arr[], int n)
 {
     int i;
-
-    // Initialize maximum element
-    double max = arr[0];
-
-    // Traverse array elements
-    // from second and compare
-    // every element with current max
+    double max = arr[0]; // Initialize maximum element.
+    // Go through every element and compare with the current max.
     for (i = 1; i < n; i++)
         if (arr[i] > max)
             max = arr[i];
@@ -72,7 +67,7 @@ double largest(double arr[], int n)
     return max;
 }
 
-// Tried to use these functions first but there was some memory issues. If
-// we implemented these functions the read file would most likely
-// sort the data much quicker then it currently does, which would help us read
-// the larger files.
+// Tried to use some other sort functions (quicksort) first but there was some
+// memory issues. If we implemented these functions the read file would most
+// likely sort the data much quicker then it currently does,
+// which would help us read the larger files.
--- Oblig_1/main.c
+++ Oblig_1/main.c
@@ -27,30 +27,19 @@ int main(int narg, char **argv){
 
   read_graph_from_file(argv[1], &N, &row_ptr, &col_idx, &val, &dangling_idx);
   printf("------------------- \n");
-
   // We can use the N to allocate memory to the scores vector:
   scores = (double*) malloc(N * sizeof(double));
-  start = clock();
   PageRank_iterations(N, row_ptr, col_idx, val, d, epsilon, scores,
     dangling_idx);
   printf("------------------------ \n");
   top_n_webpages(N, scores, n);
-  end = clock();
-  end_time = end - start;
-  printf("------------------------ \n");
-  printf("%lu ms \n.", end_time/CLOCKS_PER_SEC * 1000);
   printf("------------------------ \n");
   printf("After parallelisation:\n");
   printf("------------------------ \n");
-  start_2 = clock();
   PageRank_iterations_omp(N, row_ptr, col_idx, val, d, epsilon, scores,
     dangling_idx);
   printf("------------------------ \n");
   top_n_webpages_omp(N, scores, n);
-  end_2 = clock();
-  end_time_2 = end - start;
-  printf("------------------------ \n");
-  printf("%lu ms \n.", end_time_2/CLOCKS_PER_SEC * 1000);
 
   // Free the memory after use:
   free(row_ptr);
