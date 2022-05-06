38be374407335e8bc3d94ec6019140b4c6b2ba4c
Sebamun <SebastianAmundsen99@gmail.com>
2022-05-06 20:02:06 +0200

    ryddet vekk rot

--- Oblig_2/parallel_code/parallel_functions.c
+++ Oblig_2/parallel_code/parallel_functions.c
@@ -8,8 +8,6 @@
 int calc_m(int m, int num_procs, int rank){
   int my_start, my_stop;
   int term = m / num_procs;
-  //int rest = m % num_procs;
-  //if(rest != 0) term++;
 
   if (rank != 0){
     my_start = term * rank - 1;
@@ -49,8 +47,6 @@ int calc_new_m(int m, int num_procs, int rank){
 
 }
 
-
-
 void swapper(image *a, image *b)
 {// Used to swap position of two arrays.
   image t = *a;
@@ -59,42 +55,23 @@ void swapper(image *a, image *b)
 }
 
 void allocate_image(image *u, int m, int n){
-
   u->m = m;
- u->n = n;
-
- u->image_data = malloc(m*sizeof(float *));
- u->image_data[0] = malloc(m*n*sizeof(float));
- for (size_t i = 1; i < m; i++)
-   u->image_data[i] = &(u->image_data[i-1][n]);
-  // Here we should allocate memory for u.
-  /*
-  u->m = m; // In practise these will be the my_m and my_n values.
   u->n = n;
-  u->image_data = malloc(m * sizeof(float*));
-  for (size_t i = 0; i<m; i++){
-    (u->image_data)[i] =
-  }
-  */
+  u->image_data = malloc(m*sizeof(float *));
+  u->image_data[0] = malloc(m*n*sizeof(float));
+  for (size_t i = 1; i < m; i++) u->image_data[i] = &(u->image_data[i-1][n]);
 }
 
 void deallocate_image(image *u){
-free(u->image_data[0]);
+  free(u->image_data[0]);
   free(u->image_data);
   }
-/*
-void deallocate_image(image *u){
-  for (size_t i = 0; i<u->m; i++) free((u->image_data)[i]);
-  free(u->image_data);
-}
-*/
 
 void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
   // Want to fill our 2d array with values from 1d array.
-  // for (int i = 0; i<(u->m * u->n); i++ )printf("%d \n", image_chars[i]);
   for (size_t i = 0 ; i<u->m; i ++){
     for (size_t j = 0; j<u->n; j++){
-      (u->image_data)[i][j] = image_chars[i*u->n + j]; // add (float) before here?
+      (u->image_data)[i][j] = image_chars[i*u->n + j];
       // Here we skip the i*n for each iteration in image_chars.
     }
   }
@@ -109,19 +86,20 @@ void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
   }
 }
 
-void iso_diffusion_denoising_parallel(image *u, image *u_bar, float kappa, int iters, int my_rank, int num_procs){
+void iso_diffusion_denoising_parallel(image *u, image *u_bar,
+  float kappa, int iters, int my_rank, int num_procs){
   // Set boundary conditions in horisontal direction:
   double l_odd, l_0, l_l, l_even;
   MPI_Status sta_1, sta_2, s;
   for (int j = 0; j<(u->n); j++){ // i < n means we only go up to n-1, which is
     // what we want. In the parallised version we only want to set these
     // boundary conditions for the first and last process.
-    //if (my_rank == 0){ // First processer.
+    if (my_rank == 0){ // First processer.
       (u_bar->image_data)[0][j] = (u->image_data)[0][j];
-    //}
-    //if (my_rank == num_procs-1){ // Last processor.
+    }
+    if (my_rank == num_procs-1){ // Last processor.
       (u_bar->image_data)[(u->m)-1][j] = (u->image_data)[(u->m)-1][j];
-    //}
+    }
   }
 
   // Now we set boundary conditions in the vertical direction. This will be the
@@ -130,226 +108,104 @@ void iso_diffusion_denoising_parallel(image *u, image *u_bar, float kappa, int i
     (u_bar->image_data)[i][0] = (u->image_data)[i][0];
     (u_bar->image_data)[i][(u->n)-1] = (u->image_data)[i][(u->n)-1];
   }
-  // We divide our communication between processors in three sections. Before
+  // We divide our communication between processors in three classes of sections:
+  // the first section, the middle sections and the last section. Before
   // we can execute our algorithm we need to share some information between the
-  // processes, namely the ghostpoints. First we allocate these rows as pointers:
-
-  float *toplayer = malloc(u->n * sizeof(float));
-  float *botlayer = malloc(u->n * sizeof(float));
+  // processes, namely the ghostpoints.
 
   for (size_t idx = 0; idx<iters; idx++){
-    if (my_rank%2!=0) {
-
-              // send and receive top
-              toplayer = u->image_data[0];
-              MPI_Recv(toplayer, u->n, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD, &s);
-              MPI_Send(toplayer, u->n, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD);
-              u->image_data[1] = toplayer;
-
-              // send and receive bottom
-              if (my_rank != num_procs-1){
-                  botlayer = u->image_data[u->m - 2];
-                  MPI_Send(botlayer, u->n, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD);
-                  MPI_Recv(botlayer, u->n, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD, &s);
-                  u->image_data[u->m - 1] = botlayer;
-              }
-
-          } else { // even
-              // send and receive bottom
-              if (my_rank != num_procs-1){
-                  botlayer = u->image_data[u->m - 2];
-                  MPI_Send(botlayer, u->n, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD);
-                  MPI_Recv(botlayer, u->n, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD, &s);
-                  u->image_data[u->m - 1] = botlayer;
-              }
-
-              // send and receive top
-              if (my_rank != 0){ // since my_rank-1 does not exist for my_rank=0
-                  toplayer = u->image_data[0];
-                  MPI_Recv(toplayer, u->n, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD, &s);
-                  MPI_Send(toplayer, u->n, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD);
-                  u->image_data[1] = toplayer;
-
-              }
-
-          }
-
-    /*
-
-  if (my_rank%2){ // Go in here if odd rank.
+    if (my_rank % 2 != 0) {// Go in here if odd rank.
     // We need to send and receive from the layer above the current one,
     // which also needs to be done by the the last section:
-    // We also need to receive information from the layer above the current one:
-    MPI_Recv(toplayer, u->n, MPI_DOUBLE, my_rank-1, 0, MPI_COMM_WORLD, &sta_2);
-    (u->image_data)[0] = toplayer; // This becomes our first row (of ghostpoints).
-    toplayer = (u->image_data)[1]; // Second row.
-    // Send information to the layer above this one:
-    MPI_Send(toplayer, u->n, MPI_DOUBLE, my_rank-1, 0, MPI_COMM_WORLD);
 
-    if (my_rank != (num_procs-1)){ // Go in here if not last process. Then we also
-      // Send/Receive information to the layer below this one:
-      botlayer = (u->image_data)[(u->m-1)-1]; // Second to last row.
-      MPI_Send(botlayer, u->n, MPI_DOUBLE, my_rank+1, 0, MPI_COMM_WORLD);
-      // Receive information from the layer below this one:
-      MPI_Recv(botlayer, u->n, MPI_DOUBLE, my_rank+1, 0, MPI_COMM_WORLD, &sta_1);
-      // We can now add a extra ghost row to our section.
-      u->image_data[u->m - 1] = botlayer;
-    }
+              MPI_Recv(u->image_data[0],
+                u->n,
+                MPI_FLOAT,
+                my_rank-1,
+                0,
+                MPI_COMM_WORLD,
+                &s);
+              MPI_Send(u->image_data[1],
+                u->n,
+                MPI_FLOAT,
+                my_rank-1,
+                0,
+                MPI_COMM_WORLD);
 
+              if (my_rank != num_procs-1){// Go in here if not last process.
+              // Then we also Send/Receive information to the layer below
+              // this one:
 
-  }
-  else{ // This is if my_rank is even:
-    // We also need to send and receive from the layer below the current one,
-    // which also needs to be done by the the first section but not for the first:
-    if (my_rank != num_procs-1){
-      botlayer = (u->image_data)[(u->m-1)-1]; // Second to last row.
-      MPI_Send(botlayer, u->n, MPI_DOUBLE, my_rank+1, 0, MPI_COMM_WORLD);
-      // Receive information from the layer below this one:
-      MPI_Recv(botlayer, u->n, MPI_DOUBLE, my_rank+1, 0, MPI_COMM_WORLD, &sta_1);
-      // We can now add a extra ghost row to our section.
-      u->image_data[u->m - 1] = botlayer;
-  }
-    if (my_rank != 0){ // Go in here if my_rank is different from zero. The we
-      // also need to Send/Receive information from the layer above this one:
-      MPI_Recv(toplayer, u->n, MPI_DOUBLE, my_rank-1, 0, MPI_COMM_WORLD, &sta_2);
-      (u->image_data)[0] = toplayer; // This becomes our first row (of ghostpoints).
-      toplayer = (u->image_data)[1]; // Second row.
-      MPI_Send(toplayer, u->n, MPI_DOUBLE, my_rank-1, 0, MPI_COMM_WORLD);
-    }
-    */
+                  MPI_Send(u->image_data[u->m - 2],
+                    u->n,
+                    MPI_FLOAT,
+                    my_rank+1,
+                    0,
+                    MPI_COMM_WORLD);
+                  MPI_Recv(u->image_data[u->m - 1],
+                    u->n,
+                    MPI_FLOAT,
+                    my_rank+1,
+                    0,
+                    MPI_COMM_WORLD,
+                    &s);
 
-  //}
-    for (size_t i = 1; i<(u->m)-1; i++){
-      for (size_t j = 1; j<(u->n)-1; j++){
+              }
 
-        (u_bar->image_data)[i][j] = (u->image_data)[i][j] + \
-        kappa * ( (u->image_data)[i-1][j] + (u->image_data)[i][j-1] - \
-        4*(u->image_data)[i][j] + (u->image_data)[i][j+1] + \
-        (u->image_data)[i+1][j]);
-      }
+          } else { // Go in here if my_rank is even.
+            // We also need to send and receive from the layer below
+            // the current one, which also needs to be done by the the
+            // first section but not for the last:
+              if (my_rank != num_procs-1){
 
-    }
-    swapper(u, u_bar); // Swap pointers.
-  }
-  swapper(u, u_bar); // Swap back pointers one last time.
+                  MPI_Send(u->image_data[u->m - 2],
+                    u->n,
+                    MPI_FLOAT,
+                    my_rank+1,
+                    0,
+                    MPI_COMM_WORLD);
+                  MPI_Recv(u->image_data[u->m - 1],
+                    u->n,
+                    MPI_FLOAT,
+                    my_rank+1,
+                    0,
+                    MPI_COMM_WORLD,
+                    &s);
 
-}
+              }
 
+              // send and receive top
+              if (my_rank != 0){ // Go in here if my_rank is not zero. The we
+                // also need to Send/Receive information from the layer above
+                //this one:
 
+                  MPI_Recv(u->image_data[0],
+                    u->n,
+                    MPI_FLOAT,
+                    my_rank-1,
+                    0,
+                    MPI_COMM_WORLD,
+                    &s);
+                  MPI_Send(u->image_data[1],
+                    u->n,
+                    MPI_FLOAT,
+                    my_rank-1,
+                    0,
+                    MPI_COMM_WORLD);
+              }
+          }
 
+      for (size_t i = 1; i<(u->m)-1; i++){
+        for (size_t j = 1; j<(u->n)-1; j++){
 
-// Comment: The processor will hang when it sends a chunk of data, untill you
-// receive it in another processor.
-
-
-
-
-
-
-// Now we go to the last section, which wants to receive from values
-// from the first row in the previous section and send its first row to
-// the previous section.
-/*
-if (my_rank == (num_procs - 1)){
-  // Then we want to receive the last row from the previous section:
-  MPI_Irecv(&(u->image_data)[0][i], 1, MPI_DOUBLE, my_rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &req_1);
-  //(u->image_data)[0][i] = l_even;
-}
-if (my_rank == 0){
-  // We also want to receive the upcoming elements in the first row of the
-  // next section:
-  MPI_Irecv(&(u->image_data)[(u->m)][i], 1, MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, &req_2);
-  //(u->image_data)[u->m][i] = l_odd;
-}
-if (my_rank == (num_procs - 1)){
-  // First we send the first row back to the previous section:
-  //l_l = (u->image_data)[1][i];
-  MPI_Send(&(u->image_data)[1][i], 1, MPI_DOUBLE, my_rank-1, 0, MPI_COMM_WORLD);
-}
-// We begin with the upper section, which do not receive data from a section
-// over it. This is the case for my_rank = 0:
-if (my_rank == 0){
-  // We send the last row values in the to the next section:
-  //l_0 = (u->image_data)[u->m-1][i];
-  MPI_Send(&(u->image_data)[(u->m)-1][i], 1, MPI_DOUBLE, my_rank+1, 0, MPI_COMM_WORLD);
-}
-
-//MPI_Barrier(MPI_COMM_WORLD);
-
-if (my_rank == (num_procs - 1)){
-  MPI_Wait(&req_1, MPI_STATUS_IGNORE);
-}
-if (my_rank == 0){
-  MPI_Wait(&req_2, MPI_STATUS_IGNORE);
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*        if (my_rank%2 && my_rank != 0){
-          printf("%d", my_rank);
-          //printf("%d \n", iters);
-          //u_up_1 = (u->image_data)[0][i];
-          //MPI_Send(&u_up, 1, MPI_DOUBLE, my_rank, 0, MPI_COMM_WORLD);
+          (u_bar->image_data)[i][j] = (u->image_data)[i][j] + \
+          kappa * ( (u->image_data)[i-1][j] + (u->image_data)[i][j-1] - \
+          4*(u->image_data)[i][j] + (u->image_data)[i][j+1] + \
+          (u->image_data)[i+1][j]);
         }
 
-
-        //if {
-          //printf("%d", my_rank);
-          //MPI_Recv(&u_up, 1, MPI_DOUBLE, my_rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, &s);
-          //(u->image_data)[u->m+1][i] = u_up;
-        //}
-
-*/
-
-
-
-/*
-if (my_rank>0){
-  u_up = (u->image_data)[0][i];
-  MPI_Send(&u_up, 1, MPI_DOUBLE, my_rank, 0, MPI_COMM_WORLD);
-}
-if (my_rank == 0){
-  MPI_Recv(&u_under, 1, MPI_DOUBLE, my_rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &s);
-  (u->image_data)[u->m+1][i] = u_under;
-}
-*/
-
-
-
-
-
-
-
-
-        //(u_bar->image_data)[i][j] = (u->image_data)[i][j] + \
-        //kappa * ( (u->image_data)[i-1][j] + (u->image_data)[i][j-1] - \
-        //4*(u->image_data)[i][j] + (u->image_data)[i][j+1] + \
-        //(u->image_data)[i+1][j]);
-
-
-  /*
-  // Fill in the remainig values for u_bar:
-  for (size_t idx = 0; idx<iters; idx++){
-    for (size_t i = 1; i<(u->m)-1; i++){
-      for (size_t j = 1; j<(u->n)-1; j++){
-        (u_bar->image_data)[i][j] = (u->image_data)[i][j] + \
-        kappa * ( (u->image_data)[i-1][j] + (u->image_data)[i][j-1] - \
-        4*(u->image_data)[i][j] + (u->image_data)[i][j+1] + \
-        (u->image_data)[i+1][j]);
       }
+      swapper(u, u_bar); // Swap pointers.
     }
-    swapper(u, u_bar); // Swap pointers.
+    swapper(u, u_bar); // Swap back pointers one last time.
   }
-  swapper(u, u_bar); // Swap back pointers on last time.
-  */
--- Oblig_2/parallel_code/parallel_main.c
+++ Oblig_2/parallel_code/parallel_main.c
@@ -17,19 +17,14 @@ void import_JPEG_file (const char* filename, unsigned char** image_chars,
 void export_JPEG_file (const char* filename, const unsigned char* image_chars,
                       int image_height, int image_width,
                       int num_components, int quality);
-bool overlap_above(const int rank)
-{
-  return (rank != 0);
-}
-
 
 int main(int argc, char *argv[]){
       int m, n, c, iters;
       int my_m, my_n, my_rank, num_procs, new_my_m ;
       float kappa;
-      image u, u_bar, whole_image; // whole_image is where we want to gather all
+      image u, u_bar;
       // the contributions in the end.
-      unsigned char *image_chars, *my_image_chars, *new_my_image_chars; // kan være at jeg må flytte denne inni loopen.
+      unsigned char *image_chars, *my_image_chars, *new_my_image_chars;
       char *input_jpeg_filename, *output_jpeg_filename;
       int *sendcounts, *displs;
 
@@ -37,24 +32,19 @@ int main(int argc, char *argv[]){
       MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
       MPI_Comm_size (MPI_COMM_WORLD, &num_procs);
 
-     input_jpeg_filename = argv[1];
-     output_jpeg_filename = argv[2];
-     kappa = atof(argv[3]);
-     iters = atoi(argv[4]);
+      input_jpeg_filename = argv[1];
+      output_jpeg_filename = argv[2];
+      kappa = atof(argv[3]);
+      iters = atoi(argv[4]);
 
       if (my_rank==0) {
        import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
-       //allocate_image(&whole_image, m, n);
-       //my_image_chars = malloc(m*n * sizeof(my_image_chars));
       }
       // We now broadcast the information from the "root" to all remainig
       // processes:
-      MPI_Bcast (&iters, 1, MPI_INT, 0, MPI_COMM_WORLD);
-      MPI_Bcast (&kappa, 1, MPI_INT, 0, MPI_COMM_WORLD);
       MPI_Bcast (&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
       MPI_Bcast (&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
       /* 2D decomposition of the m x n pixels evenly among the MPI processes */
-
       if (my_rank == 0){
 
         sendcounts =  malloc(num_procs * sizeof(int));
@@ -64,15 +54,12 @@ int main(int argc, char *argv[]){
         for (int rank = 0; rank<num_procs; rank ++){
           my_m = calc_m(m, num_procs, rank);
           sendcounts[rank] = my_m * n;
-          printf("%d \n", sendcounts[rank]);
         }
         for (int rank = 1; rank < num_procs; rank++){
 
           displs[rank] = displs[rank-1] + sendcounts[rank - 1];
-          //displs[rank] = displs[rank] - n;
-          //if (rank != (num_procs-1)) displs[rank] = displs[rank] - n;
-          //if (rank != 0) displs[rank] = displs[rank] - n;
-          //if (rank-1 != (num_procs-1)) displs[rank] = displs[rank] - n;
+          displs[rank] = displs[rank] - n;
+          if (rank != (num_procs-1)) displs[rank] = displs[rank] - n;
         }
       }
 
@@ -80,7 +67,7 @@ int main(int argc, char *argv[]){
       // last section as it would be overwritten in the first processor if not.
 
       my_image_chars = malloc(my_m*n * sizeof (my_image_chars));
-      //printf("%d \n", my_m*n);
+
       MPI_Scatterv(image_chars,
         sendcounts,
         displs,
@@ -89,29 +76,27 @@ int main(int argc, char *argv[]){
         my_m*n,
         MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
-        // Allocation of u and u_bar:
-        allocate_image (&u, my_m, n);
-        allocate_image (&u_bar, my_m, n);
+      // Allocation of u and u_bar:
+      allocate_image (&u, my_m, n);
+      allocate_image (&u_bar, my_m, n);
 
-       convert_jpeg_to_image (my_image_chars, &u);
-      iso_diffusion_denoising_parallel (&u, &u_bar, kappa, iters, my_rank, num_procs);
+      convert_jpeg_to_image (my_image_chars, &u);
+      iso_diffusion_denoising_parallel (&u, &u_bar, kappa, iters, my_rank
+        , num_procs);
 
       convert_image_to_jpeg(&u_bar, my_image_chars);
-      //COMMENT: For imorgen- prøv å omstokke på send og receive. + sjekk convert_jpeg_to_image
-      // i tilfellet jeg er nødt til å sette start stop betingelser + offset inni my_image_chars
 
        // We now need to adjust our send_counts and displs:
 
-       if (my_rank == 0){
-         displs[0] = 0;
-         for (int rank = 0; rank<num_procs; rank ++){
-           new_my_m = calc_new_m(m, num_procs, rank);
-           sendcounts[rank] = new_my_m * n;
-         }
-         for (int rank = 1; rank < num_procs; rank++)
-           displs[rank] = displs[rank-1] + sendcounts[rank-1];
-       }
-
+      if (my_rank == 0){
+        displs[0] = 0;
+        for (int rank = 0; rank<num_procs; rank ++){
+          new_my_m = calc_new_m(m, num_procs, rank);
+          sendcounts[rank] = new_my_m * n;
+        }
+        for (int rank = 1; rank < num_procs; rank++)
+          displs[rank] = displs[rank-1] + sendcounts[rank-1];
+        }
 
        // We now need to revert back to the original actual number of rows for
        // each section:
@@ -119,151 +104,32 @@ int main(int argc, char *argv[]){
        new_my_image_chars = malloc(new_my_m*n * sizeof (my_image_chars));
 
        if (my_rank == 0){
-         for (int i = 0; i<new_my_m*n;i++) new_my_image_chars[i] = my_image_chars[i];
+         for (int i = 0; i<new_my_m*n;i++){
+            new_my_image_chars[i] = my_image_chars[i];
+         }
        }
       if (my_rank != 0){
-        for (int i = 0; i<new_my_m*n;i++) new_my_image_chars[i] = my_image_chars[i + n];
+        for (int i = 0; i<new_my_m*n;i++){
+          new_my_image_chars[i] = my_image_chars[i + n];
+        }
       }
 
-
-       //convert_image_to_jpeg(&u_bar, my_image_chars);
-       MPI_Gatherv(new_my_image_chars, new_my_m*n, MPI_UNSIGNED_CHAR, image_chars, sendcounts, displs, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-
-       //MPI_Gatherv(&(my_image_chars[overlap_above(my_rank)*n]), new_my_m*n, MPI_UNSIGNED_CHAR, image_chars, sendcounts, displs, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-       //printf("hei\n");
-
-         /* each process sends its resulting content of u_bar to process 0 */
-         /* process 0 receives from each process incoming values and */
-         /* copy them into the designated region of struct whole_image */
-         /*  ...  */
-        //convert_image_to_jpeg(&u_bar, my_image_chars);
-        //MPI_Gatherv(my_image_chars, n_rows[my_rank]*n, MPI_UNSIGNED_CHAR ,image_chars ,sendcounts ,Gdispls ,MPI_UNSIGNED_CHAR ,0 ,MPI_COMM_WORLD);
+       MPI_Gatherv(new_my_image_chars,
+         new_my_m*n,
+         MPI_UNSIGNED_CHAR,
+         image_chars,
+         sendcounts,
+         displs,
+         MPI_UNSIGNED_CHAR,
+         0,
+         MPI_COMM_WORLD);
 
         if (my_rank==0) {
-          //convert_image_to_jpeg(&whole_image, image_chars);
           export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
-           //deallocate_image (&whole_image);
         }
         deallocate_image (&u);
         deallocate_image (&u_bar);
 
-
-
         MPI_Finalize ();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      //int rows = m/num_procs; // The number of rows for each process.
-      //int remainder = m%num_procs; // Get value if not divisible by num_procs.
-      // We can now collect the m and n values for each process:
-
-
-
-      //my_m = my_m + ((my_rank >= (num_procs - remainder)) ? 1:0);
-      //my_n = n;
-
-      // Allocate memory for arrays to be used in scatter and gather functions:
-      //int *n_rows = malloc(num_procs * sizeof(int)); // Store number of rows for each process.
-      //int *sendcounts = (int*) malloc(num_procs * sizeof(int));
-      //int *displs = (int*) malloc(num_procs * sizeof(int));
-      //int *Gdispls = malloc(num_procs * sizeof *Gdispls);
-
-      /*
-      displs[0] = 0;
-      Gdispls[0] = 0;
-
-      n_rows[0] = rows + ((0 >= (num_procs - remainder)) ? 1:0) + 1;
-      sendcounts[0] = n_rows[0]*n;
-      displs[1] = displs[0] + sendcounts[0] - ((n_rows[0]-rows)*n);
-
-
-      for (int rank = 1; rank<num_procs-1; rank++){
-        n_rows[rank] = rows + ((rank >= (num_procs - remainder)) ? 1:0) + 2; // Fill number of rows for each process.
-        sendcounts[rank] = n_rows[rank]*n; // Fill number of values for each process.
-        displs[rank+1] = displs[rank] + sendcounts[rank] - ((n_rows[rank]-rows)*n); // Starting point for each process.
-        Gdispls[rank+1] = Gdispls[rank] + n_rows[rank];
-      }
-
-      // Need to take care of last row and number of values:
-      n_rows[num_procs-1] = rows + ((num_procs-1) >= (num_procs - remainder) ? 1:0) + 1;
-      sendcounts[num_procs-1] = n_rows[num_procs-1]*n;
-      displs[num_procs-1] = displs[num_procs-1] + sendcounts[num_procs-1] - ((n_rows[num_procs-1]-rows)*n);
-      printf("%d \n", n_rows[num_procs-1]-rows);
-
-      */
-
-      // Allocation of u and u_bar:
-      //allocate_image (&u, n_rows[my_rank], my_n);
-      //allocate_image (&u_bar, n_rows[my_rank], my_n);
-
-
-/* This was wrong:
-      if (my_rank == 0) {
-        //my_image_chars = malloc(m*n * sizeof *my_image_chars);
-        my_image_chars = malloc(n_rows[my_rank]*n * sizeof *my_image_chars);
-      }
-      else my_image_chars = malloc(n_rows[my_rank]*n * sizeof *my_image_chars);
-      */
-
-      //my_image_chars = malloc(n_rows[my_rank]*n * sizeof (my_image_chars));
-
-/*
-      MPI_Scatterv(image_chars,
-        sendcounts,
-        displs,
-        MPI_UNSIGNED_CHAR,
-        my_image_chars,
-        n_rows[my_rank]*n,
-        MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-  printf("hei \n");
-*/
-
-
-      //my_image_chars = (unsigned char*) malloc(my_m*my_n * sizeof(unsigned char));
-
-      //MPI_Gather(&size, 1, MPI_INT, sendcounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-      //for (int i = 0; i<num_procs; i++){
-        //displs[i] = sendcounts[i]*i;
-        //printf("%d \n", displs[i]);
-      //}
-      //printf("%d \n", displs[0]);
-      //allocate_image (&u, my_m, my_n);
-      //allocate_image (&u_bar, my_m, my_n);
-      //each process asks process 0 for a partitioned region
-      //of image_chars and copy the values into u
-      //MPI_Scatterv(image_chars, sendcounts, displs, MPI_UNSIGNED_CHAR, my_image_chars, my_m*my_n, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-
-      //convert_jpeg_to_image (my_image_chars, &u);
-
-      //iso_diffusion_denoising_parallel (&u, &u_bar, kappa, iters, my_rank, num_procs);
-
-       /* each process sends its resulting content of u_bar to process 0 */
-       /* process 0 receives from each process incoming values and */
-       /* copy them into the designated region of struct whole_image */
-       /*  ...  */
-      //convert_image_to_jpeg(&u_bar, my_image_chars);
-      //MPI_Gatherv(my_image_chars, n_rows[my_rank]*n, MPI_UNSIGNED_CHAR ,image_chars ,sendcounts ,Gdispls ,MPI_UNSIGNED_CHAR ,0 ,MPI_COMM_WORLD);
-
-      //if (my_rank==0) {
-      //convert_image_to_jpeg(&whole_image, image_chars);
-      //   export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
-      //   deallocate_image (&whole_image);
-      //}
-      //deallocate_image (&u);
-      //deallocate_image (&u_bar);
 
 return 0; }
